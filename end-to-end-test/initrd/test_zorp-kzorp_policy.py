b2577a532d98b48787419a4c2702f6bd8d95432e
Mag <mag@magwas.rulez.org>
2013-05-22 17:40:34 +0200

    GPL v2+, COPYING mentions files with other copyrights #5

--- end-to-end-test/initrd/test_zorp-kzorp_policy.py
+++ end-to-end-test/initrd/test_zorp-kzorp_policy.py
@@ -7,7 +7,7 @@
 # Copyright (C) 2006-2012, BalaBit IT Ltd.
 # This program/include file is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as published
-# by the Free Software Foundation; either version 3 of the License, or
+# by the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
 # This program/include file is distributed in the hope that it will be
